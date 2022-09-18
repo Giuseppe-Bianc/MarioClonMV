8e1b5039eb5ce43e5db72956cc4644c311307ee3
Giuseppe-Bianc <75339264+Giuseppe-Bianc@users.noreply.github.com>
2022-09-18 19:00:23 +0200

    eliminazione

--- src/main/java/util/Consts.java
+++
@@ -1,6 +0,0 @@
-package util;
-
-public class Consts {
-	public static final String DEFAULTSHADER =  "src/main/resources/assets/shaders/defaultShader.glsl" ;
-	public static final String SPRITESHEET = "src/main/resources/assets/images/spritesheet.png";
-}
